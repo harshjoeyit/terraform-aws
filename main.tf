9c0e5207fc4f79c120d84339b0571377ce661199
harshjoeyit <harshit02gangwar@gmail.com>
2025-09-07 12:24:41 +0530

    added a VPC

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
+}
+
 # Get info about AWS AMI (Amazon Machine Image) for Ubuntu 24.04 LTS
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -17,9 +31,12 @@ data "aws_ami" "ubuntu" {
 # id of the instance -> aws_instance.app_server
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
-    Name = "learn-terraform"
+    Name = var.instance_name
   }
 }
